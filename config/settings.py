173f18f876b77a799c5c52ebf0809bd1b9240c39
nikolyai-tarasov <kolya.tarasov333@gmail.com>
2024-12-15 00:55:31 +0300

    final_

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "users",
+    "materials",
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
@@ -122,7 +123,7 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media',)
 
 AUTH_USER_MODEL = 'users.User'
