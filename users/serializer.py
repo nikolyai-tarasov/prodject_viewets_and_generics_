df6df180833b1b26bd133fd1cdc4c33f7bc3b45e
nikolyai-tarasov <kolya.tarasov333@gmail.com>
2025-01-09 14:40:09 +0300

    final_z

--- users/serializer.py
+++ users/serializer.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 from users.models import Payments
 
+
+
+class PaymentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payments
+        fields = '__all__'
--- users/views.py
+++ users/views.py
@@ -3,25 +3,29 @@ from rest_framework import generics
 from users.models import Payments
 from rest_framework.filters import OrderingFilter
 
+from users.serializer import PaymentsSerializer
+
 
 class PaymentsCreateAPIView(generics.CreateAPIView):
-    pass
+    serializer_class = PaymentsSerializer
 
 
 class PaymentsListAPIView(generics.ListAPIView):
+    serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
+
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = ('paid_course','paid_lesson','payment_method',)
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method',)
     ordering_fields = ('pay_date',)
 
 
-
-
 class PaymentsRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
 
 
 class PaymentsUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
 
 
